b4b85a5fa82a779c417a13163a4872c0aaa72248
Hunter <huntermonster8@gmail.com>
2019-11-30 14:44:19 -0800

    Initial Commit
    Initial commit of my extra credit project

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-AP_Physics_Extra_Credit
+# AP Physics Mech A Extra Credit
+
+![](demo.gif)
+This is an extra credit assignment I designed for my AP Physics class at HBHS. Its made of 1000 3D rigid bodies textured as boxes.  
+
+# Design
+
+So my AP Physics teacher assigned an extra credit assignment to do a flip book related to physics.  I don't have any artistic ability, so I decided to use the game engine ![https://https://godotengine.org/](Godot) and set up a physics related scene. 
+This scene consists of 1000 rigid bodies stacked up in a tower formation and uses the Bullet Physics engine to simulate physics.  To start the scene, I roll a granite ball toward the tower, which causes it to collapse.
+
+**press the spacebar to launch the marble**
+
+Using OBS Studio, I recorded and made a video, which I then converted to GIF and then uploaded to gifprint.com to convert it to a PDF flipbook.
+
+After doing all this work, I realize it might have just been easier to hand draw 100 pages of a flip book animation. Oh well, it's done!
+
+
+### Credits
+
+  - My dad for the idea and original starting code.
+  - Brackeys for the nice wooden box textures.
+  - Mr. DeWilde for being a cool AP Physics teacher!
+  - Mr. Crossett for helping me become a better programmer. 
+
+
+License
+----
+
+MIT License (MIT)
+
+Copyright (c) 2019 Hunter Ruebsamen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ scripts/main.gd
@@ -0,0 +1,90 @@
+extends Spatial
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export(int,20,75000) var num_cubes := 500
+export(int,12,60) var size := 20
+
+#distance between the cubes
+var x_dist : float = 2.0
+var y_dist : float = 2.0
+var z_dist : float = 2.0
+
+onready var cube_mm : PackedScene = preload("res://scenes/cube_mm.tscn")
+onready var marble : PackedScene = preload("res://scenes/marble.tscn")
+onready var sleepLabel : Label = $CanvasLayer/SleepLabel
+onready var cubeContainer : Spatial = $CubeContainer
+onready var mm : MultiMesh = $MultiMeshInstance.get_multimesh()
+var instanced_marble
+
+var marble_launched : bool = false
+var camera_anglev : int = -15
+const mouse_sens : float  = 0.2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	instanced_marble = marble.instance()
+	add_child(instanced_marble)
+	$CanvasLayer/HSlider.value = num_cubes
+	$CanvasLayer/NumLabel.text = "cubes: " + str(num_cubes) + " (" + str(num_cubes) + ")"
+	# setup multimesh instances
+	mm.set_instance_count(num_cubes)
+	spawnCubes()
+
+func _input(event : InputEvent) -> void:         
+	if event is InputEventMouseMotion && Input.is_mouse_button_pressed(BUTTON_LEFT):
+		$Camera.rotate_y(deg2rad(-event.relative.x*mouse_sens))
+		var changev=-event.relative.y*mouse_sens
+		if camera_anglev+changev>-50 and camera_anglev+changev<50:
+			camera_anglev+=changev
+			$Camera.rotate_x(deg2rad(changev))
+	if event is InputEventKey:
+		if Input.is_key_pressed(KEY_SPACE):
+			launchMarble()
+
+func spawnCubes() -> void:
+	for i in range(mm.instance_count):
+		var instanced_cube = cube_mm.instance()
+		var modval = i%size
+# warning-ignore:integer_division
+		if (modval < size/4):
+			instanced_cube.translate(Vector3(modval*x_dist,i/size*y_dist,40))
+# warning-ignore:integer_division
+		elif (modval < size/2):
+			instanced_cube.translate(Vector3(0,i/size*y_dist,40+z_dist+((modval-size/4)*z_dist)))
+# warning-ignore:integer_division
+		elif (modval < size/4*3):
+			instanced_cube.translate(Vector3(size/2-x_dist,i/size*y_dist,40+z_dist+((modval-size/2)*z_dist)))
+		else:
+			instanced_cube.translate(Vector3((modval-size/4*3)*x_dist,i/size*y_dist,40+z_dist+size/4*z_dist))
+		get_node("CubeContainer").add_child(instanced_cube)
+		mm.set_instance_transform(i,instanced_cube.transform)
+
+func launchMarble() -> void:
+	if not marble_launched:
+		instanced_marble.add_central_force(Vector3(-2000,0,-19000))
+		marble_launched = true
+
+func deleteCubes() -> void:
+	for c in cubeContainer.get_children():
+		c.free()
+
+func resetAll() -> void:
+	cubeContainer.resetFPS()
+	# delete marble
+	instanced_marble.free()
+	
+	# delete cubes
+	deleteCubes()
+	num_cubes = int($CanvasLayer/HSlider.value)
+	_on_HSlider_value_changed(num_cubes)
+	mm.set_instance_count(num_cubes)
+	# reinstance marble
+	instanced_marble = marble.instance()
+	marble_launched = false
+	add_child(instanced_marble)
+	spawnCubes()
+
+func _on_HSlider_value_changed(value : int) -> void:
+	$CanvasLayer/NumLabel.text = "cubes: " + str(value) + " (" + str(num_cubes) + ")"
